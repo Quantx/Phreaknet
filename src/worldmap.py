e55b1e618eeb6eee46e36d6ecd97baaa54d9aba0
Sam <sam.e.deutsch@gmail.com>
2018-03-18 20:26:24 -0400

    Higher resolution map

--- src/worldmap.py
+++ src/worldmap.py
@@ -25,20 +25,29 @@ class Worldmap( Program ):
         self.wm_sf = shapefile.Reader( worldmap_file )
         # The bytearray to use while drawing
         self.wm_bar = None
+        # The transforms
+        self.wm_xmin = self.wm_sf.bbox[0]
+        self.wm_ymin = self.wm_sf.bbox[1]
+        # The sizes
+        self.wm_width  = self.wm_sf.bbox[2] - self.wm_xmin
+        self.wm_height = self.wm_sf.bbox[3] - self.wm_ymin
+        # The skews
+        self.wm_xskw = 1
+        self.wm_yskw = 1
+        # The ASCII charater map
+        self.wm_cmap = [ " ", ".", ",", "_", "'", "|", "/", "J",
+                         "`", "\\","|", "L", "\"","7", "r", "o" ]
 
     def run( self ):
-        # Configure the bytearray
-        self.wm_bar = BitArray( self.size[0] * self.size[1] )
+        # Configure the bytearray, 4 times the screen size
+        self.wm_bar = BitArray( self.size[0] * self.size[1] * 4 )
         # Abort if we cannot read this file
         if self.wm_sf.shapeType != shapefile.POLYGON:
             self.error( "unable to read this map file type" )
             return self.kill
-        # Calculate the transforms
-        self.wm_xmin = self.wm_sf.bbox[0]
-        self.wm_ymin = self.wm_sf.bbox[1]
         # Calculate the skews
-        self.wm_xskw = ( self.wm_sf.bbox[2] - self.wm_xmin ) / self.size[0]
-        self.wm_yskw = ( self.wm_sf.bbox[3] - self.wm_ymin ) / self.size[1]
+        self.wm_xskw = self.wm_width / (self.size[0] * 2)
+        self.wm_yskw = self.wm_height / (self.size[1] * 2)
         # Loop through all the polygons
         for shp in self.wm_sf.shapes( ):
             # Skip non polygon shapes
@@ -63,14 +72,23 @@ class Worldmap( Program ):
                 lpnt = pnt
             # Finish last polyon
             self.setLine( lpnt[0], lpnt[1], fpnt[0], fpnt[1] )
-        # Draw img for debug
-        for y in range( 0, self.size[1] ):
+        # We're done here
+        return self.drawmap( )
+
+    # Draw the ASCII representation of this map
+    def drawmap( self ):
+        # Iterate through the entire screen
+        for y in range( 0, self.size[1] * 2, 2 ):
             aln = ""
-            for x in range( 0, self.size[0] ):
-                if self.getPix( x, y ):
-                    aln += "*"
-                else:
-                    aln += " "
+            for x in range( 0, self.size[0] * 2, 2 ):
+                # Find the character represented by each 2x2 cell
+                char  = self.getPix( x, y ) * 8
+                char += self.getPix( x + 1, y ) * 4
+                char += self.getPix( x, y + 1 ) * 2
+                char += self.getPix( x + 1, y + 1 )
+                # Add the char to this line
+                aln += self.wm_cmap[char]
+            # Print this line
             self.println( aln )
         # We're done here
         return self.kill
@@ -79,16 +97,16 @@ class Worldmap( Program ):
     def setPix( self, x, y ):
         # Normalize
         x = round( x )
-        y = round( self.size[1] - y )
+        y = round( ( self.size[1] * 2 ) - y )
         # Make sure this is in bounds
-        if x > 0 and x < self.size[0] and y > 0 and y < self.size[1]:
+        if x > 0 and x < self.size[0] * 2 and y > 0 and y < self.size[1] * 2:
             # Flip the bit
-            self.wm_bar[y * self.size[0] + x] = 1
+            self.wm_bar[y * ( self.size[0] * 2 ) + x] = 1
 
     # Get a pixel
     def getPix( self, x, y ):
         # Return the bit
-        return self.wm_bar[y * self.size[0] + x]
+        return self.wm_bar[y * ( self.size[0] * 2 ) + x]
 
     # Bresenham's line algorithm
     # https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
